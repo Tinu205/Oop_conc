39213a3c7795a360832e1f393483cc80faf32438
Tinu205 <dazleroger404@gmail.com>
2023-07-19 20:07:37 +0530

    Something new

--- oop.py
+++ oop.py
@@ -4,4 +4,26 @@ class Person:
     def greet(self):
         return f"Hello Mr.{self.name}"
 jeff = Person("Zakku")
-print(jeff.greet())
+# print(jeff.greet())
+
+class pet:
+    #tricks = []# dont use this since this is shared by all instances 
+    type = "dog" #class variable shared by all instances
+    def __init__(self,name) -> None:
+        self.name = name
+        self.tricks = []
+    def add_tricks(self,trick):
+        self.tricks.append(trick)
+        
+hel = pet("Panda")
+bul = pet("snake")
+#print(hel.type)#shared by all pets
+# print(hel.name)#unique to hel
+
+# print(bul.type)
+# print(bul.name)
+
+hel.add_tricks("Rollover")
+
+print(hel.tricks)
+print(bul.tricks)

fc281afdff883fd8b26167f167783944045eb4e4
Tinu205 <dazleroger404@gmail.com>
2023-07-22 19:55:10 +0530

    Learne a lot new stuff

--- oop.py
+++ oop.py
@@ -48,4 +48,33 @@ class sound(pet):
 # print(isinstance(kutty,pet))
 # print(isinstance(kutty,sound))
 
+# Encapsulation and access modifers
+class person:
+    def __init__(self,name,age) -> None:
+        self._name = name #protected that's can be view but can't be changed
+        self.__age = age #can't be viwed or changed
+        pass
+    def get(self):
+        return f"{self._name} is {self.__age} years old."
+    
+    @property
+    def age(self):
+        return self.__age
+    
+    @age.setter
+    def newage(self,newage):
+        self.__age = newage
+paari = person("Paari",22)
+print(paari.get())
 
+#print(paari.__age) wont work because it's protected
+# print(paari.age)
+# paari.__age = 12 #This wont work
+# print(paari.age)
+# paari.newage = 40
+# print(paari.age) #Now the value is upadated
+
+#### Class relaitonships ####
+## Associations 
+## Aggrigations
+## compotions """Creating a instance of a class inside another class"""

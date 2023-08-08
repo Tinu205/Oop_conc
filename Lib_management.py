5183120971805ac44da6383241e9a301407ea46c
Tinu205 <dazleroger404@gmail.com>
2023-08-08 17:01:35 +0530

    Added todos

---
+++ Lib_management.py
@@ -0,0 +1,74 @@
+# TODO: Define Classes
+
+# TODO: Library Class
+# - Attributes: name, location, list_of_books, list_of_patrons
+# - Methods: add_book(book), remove_book(book), add_patron(patron), remove_patron(patron), display_books(), display_patrons()
+
+# TODO: Book Class
+# - Attributes: title, author, genre, publication_year, ISBN, is_available
+# - Methods: mark_as_available(), mark_as_unavailable(), display_info()
+
+# TODO: Patron Class
+# - Attributes: name, contact_info, membership_status
+# - Methods: update_contact_info(info), update_membership_status(status), display_info()
+
+# TODO: Transaction Class
+# - Attributes: book, patron, transaction_date
+# - Methods: None (attributes to record transaction details)
+
+# TODO: Define Class Relationships
+
+# TODO: Library
+# - Relationship: A Library has a list of books and patrons.
+
+# TODO: Book
+# - Relationship: A Book is part of a Library's collection and can have multiple Transactions.
+# - Relationship: A Book can be marked as available/unavailable.
+
+# TODO: Patron
+# - Relationship: A Patron is a member of a Library and can have multiple Transactions.
+# - Relationship: A Patron can update their contact info and membership status.
+
+# TODO: Transaction
+# - Relationship: A Transaction involves a specific Book and a specific Patron.
+
+# TODO: Implement Methods and Attributes
+
+# TODO: Library Class
+# - Method: add_book(book) - Adds a book to the list_of_books.
+# - Method: remove_book(book) - Removes a book from the list_of_books.
+# - Method: add_patron(patron) - Adds a patron to the list_of_patrons.
+# - Method: remove_patron(patron) - Removes a patron from the list_of_patrons.
+# - Method: display_books() - Displays information about available books.
+# - Method: display_patrons() - Displays information about patrons.
+
+# TODO: Book Class
+# - Method: mark_as_available() - Sets is_available attribute to True.
+# - Method: mark_as_unavailable() - Sets is_available attribute to False.
+# - Method: display_info() - Displays detailed information about the book.
+
+# TODO: Patron Class
+# - Method: update_contact_info(info) - Updates the contact_info attribute.
+# - Method: update_membership_status(status) - Updates the membership_status attribute.
+# - Method: display_info() - Displays detailed information about the patron.
+
+# TODO: Transaction Class
+# - Attributes: Holds attributes for book, patron, transaction_date.
+
+# TODO: Additional Steps
+
+# TODO: Create instances of Library, Book, and Patron classes to simulate interactions.
+
+# TODO: Implement user interfaces to interact with the library system.
+
+# TODO: Add error handling to ensure smooth execution and user-friendly experience.
+
+# TODO: Implement data persistence using file handling or a database.
+
+# TODO: Write comprehensive unit tests to validate class methods.
+
+# TODO: Create clear and concise documentation explaining class purpose and usage.
+
+# TODO: Summarize project goals and how it demonstrates understanding of OOP concepts.
+
+# TODO: Conclude the project description with a thank you or final thoughts.

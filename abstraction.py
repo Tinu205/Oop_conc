a32b938d76920e2e4ded7f6501d74ad407a190f0
Tinu205 <dazleroger404@gmail.com>
2023-08-17 17:49:46 +0530

    Added comments

--- abstraction.py
+++ abstraction.py
@@ -3,12 +3,12 @@ from abc import ABC, abstractmethod
 class Animal(ABC):
     @abstractmethod
     def make_sound(self):
-        pass # no use but will be inherited in feature using method overridig
+        pass # no use but will be inherited in feature using method overriding
 
 class Dog(Animal):
 
     def make_sound(self):
-        print("Woof, Woof")
+        print("Woof, Woof")# the updated method
 
 class Cat(Animal):
     def make_sound(self):
